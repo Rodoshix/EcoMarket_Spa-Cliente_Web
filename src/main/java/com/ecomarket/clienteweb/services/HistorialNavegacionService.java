7721036fd64f79a941d058cf98d71e0f4005fd6a
Rodoshix <102111944+Rodoshix@users.noreply.github.com>
2025-05-14 22:49:56 -0400

    Update HistorialNavegacionService.java

--- src/main/java/com/ecomarket/clienteweb/services/HistorialNavegacionService.java
+++ src/main/java/com/ecomarket/clienteweb/services/HistorialNavegacionService.java
@@ -8,11 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
+@Transactional 
 public class HistorialNavegacionService {
 
     @Autowired
